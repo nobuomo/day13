d1e2ca1368e052391d4ec572ac1ddb8afa785960
ashimo31 <nobuom@MoriNobuos-MacBook-Air.local>
2016-08-28 18:16:33 +0900

    day13

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :poems ,only: [:show, :update, :destroy, :edit, :new, :create]
-  root 'poems#index'
+  resources :poems,  only: [ :index, :show, :update, :destroy, :edit, :new, :create]
+    root 'poems#index'
 end
+
+#resources :poems ,only: [:show, :update, :destroy, :edit, :new, :create]
+#root 'poems#index'
